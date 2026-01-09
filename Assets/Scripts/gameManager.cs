29a58026cd67613a600b5a8f0f45912872cabadc
Lorenzo McGee <renfo17@gmail.com>
2026-01-09 13:48:28 -0600

    UI started
    Created UI (Added pause Menu)

---
+++ Assets/Scripts/gameManager.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+{
+    public static gameManager instance;
+
+    [SerializeField] GameObject menuActive;
+    [SerializeField] GameObject menuPause;
+    [SerializeField] GameObject menuWin;
+    [SerializeField] GameObject menuLose;
+    [SerializeField] GameObject menuHome;
+
+    public bool isPaused;
+
+    float timeScaleOrig;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+        instance = this;
+        timeScaleOrig = Time.timeScale;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        { 
+            if(menuActive == null)
+            {
+                statePaused();
+                menuActive = menuPause;
+
+            }
+            else if(menuActive == menuPause)
+            {
+                stateUnpaused();
+            }
+
+        }
+
+
+
+
+    }
+
+
+    public void statePaused()
+    {      
+        isPaused = true;
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+
+    public void stateUnpaused()
+    {
+        isPaused = false;
+        Time.timeScale = timeScaleOrig;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        menuActive.SetActive(false);
+        menuActive = null;
+    }
+
+
+
+}
