222d60753c63378577314fa3ecf45331fd04d499
emweston <emweston@student.fullsail.edu>
2026-01-27 22:46:27 -0800

    WinZone articulation
    Clarify level goal using WinZone naming/comments

--- Assets/TriggerWinZone.cs
+++ Assets/TriggerWinZone.cs
@@ -1,5 +1,9 @@
 using UnityEngine;
 
+
+// WinZone defines a level completion area for Graveyard.
+// When the player enters this volume, the win condition is triggered.
+
 public class TriggerWinZone : MonoBehaviour //can be attached to a GameObject in the scene
 {
     private void OnTriggerEnter(Collider other) //Unity calls this automatically when something enters the trigger
