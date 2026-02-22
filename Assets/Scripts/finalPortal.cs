da961bd77f90c20883b1f06444c89ad36c1bac1d
Lorenzo McGee <renfo17@gmail.com>
2026-02-22 01:09:08 -0600

    Portal Update

--- Assets/Scripts/finalPortal.cs
+++ Assets/Scripts/finalPortal.cs
@@ -6,9 +6,14 @@ public class finalPortal : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
-        {
+        { 
+            
+            // Save the player's inventory data
+            gameManager.instance.currentPlayerData.SaveData(InventoryManager.instance);
+
             // Load the next scene
             transitionsManager.instance.LoadScene(2, "CrossFade");
+           
         }
     }
 }
